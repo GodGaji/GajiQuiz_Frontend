e629a2459bc0f798d8a9b9320275042a60c25060
riyenas0925 <riyenas0925@gmail.com>
2021-01-31 20:55:40 +0900

    chore : merge conflict 오류 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Pages from './pages';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Pages />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
